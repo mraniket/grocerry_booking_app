216285268f84b570d8a3b310f7b668f72b5b6629
Aniket Shinde <62549345+mraniket@users.noreply.github.com>
2025-07-18 11:31:27 +0530

    Update Scan_Checkmarx.groovy

--- Scan_Checkmarx.groovy
+++ Scan_Checkmarx.groovy
@@ -13,7 +13,7 @@ def call(def appName="ESP", def costCenter="0977", def cmInclude="**/*", def cmE
     }
     // def debugging = (DebugLevel() >= Debug.high())
 //checkmarxASTScanner additionalOptions: '', baseAuthUrl: '', branchName: '', checkmarxInstallation: 'Checkmarx', credentialsId: 'CheckmarxPOV', projectName: 'projectName', serverUrl: 'https://serverURL', tenantName: 'bcbst-prod', useOwnServerCredentials: true
-    checkmarxASTScanner additionalOptions: "${scanType}--file-filter !das,!.jazz5,!tmp --report-format summaryJSON,summaryHTML,pdf --output-path "C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\test_jenkins_plugin" -s ${includeFiles} --project-groups ${costCenter} --tags BuildId:${BUILD_DISPLAY_NAME} --project-tags cc:${costCenter}", 
+    checkmarxASTScanner additionalOptions: "${scanType}--file-filter !das,!.jazz5,!tmp --report-format summaryJSON,summaryHTML,pdf --output-path \"C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\test_jenkins_plugin\" -s ${includeFiles} --project-groups ${costCenter} --tags BuildId:${BUILD_DISPLAY_NAME} --project-tags cc:${costCenter}", 
         useAuthenticationUrl: true,
         baseAuthUrl: 'https://eu.iam.checkmarx.net', 
         branchName: 'NA', 
